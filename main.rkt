4cab777a9d1bcb185cb4048fe2f2379c62e6a369
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-12-26 22:34:23 +0800

    direct convert syntax

--- main.rkt
+++ main.rkt
@@ -1,74 +1,43 @@
-#lang racket
+#lang racket/base
 
-(require data/monad data/applicative)
-(require megaparsack megaparsack/text)
+(require syntax/parse
+         nanopass/base)
 
-(struct ast () #:transparent)
-(struct let ast (name expr) #:transparent)
+(define (stx-number? stx)
+  (if (syntax? stx)
+      (number? (syntax-e stx))
+      #f))
 
-(struct expr () #:transparent)
-(struct call expr (f arg*) #:transparent)
-(struct binary expr (op lhs rhs) #:transparent)
+(define-language L0
+  (terminals
+   (identifier (name param))
+   (stx-number (num)))
+  (Expr (expr)
+        num
+        name
+        (let name expr)
+        (λ (param* ...) expr)
+        (expr expr* ...)))
 
-(define lexeme/p
-  ;;; lexeme would take at least one space or do nothing
-  ; hidden/p helps parser won't report something like: expected a space/p by hidden it
-  (do (hidden/p (or/p (many+/p space/p) void/p))
-    (pure (λ () 'lexeme))))
-(define must/lexeme/p
-  ;;; must/lexeme treat cannot lexme as error
-  (do (hidden/p (many+/p space/p))
-    (pure (λ () 'lexeme))))
+(define-pass parse : * (stx) -> L0 ()
+  (Expr : * (stx) -> Expr (expr)
+        (syntax-parse stx
+          #:literals (let λ)
+          ; let form
+          [(let name:id expr)
+           `(let ,#'name ,(parse #'expr))]
+          ; lambda form
+          [(λ (param*:id ...) expr)
+           `(λ (,(syntax->list #'(param* ...)) ...) ,(parse #'expr))]
+          [(f arg* ...)
+           `(,(parse #'f) ,(map parse (syntax->list #'(arg* ...))) ...)]
+          ; literal expression
+          [x #:when (or (identifier? stx)
+                        (stx-number? stx))
+             #'x]
+          [else (error 'syntax "unknown form: ~a" stx)]))
+  (Expr stx))
 
-(define (keyword/p keyword)
-  (do (string/p keyword)
-    (lexeme/p)
-    (pure keyword)))
-(define identifier/p
-  (do [id <- (syntax-box/p (many+/p letter/p))]
-    (lexeme/p)
-    ; rewrap output, from `(many+/p letter/p)` we would a list of char, using `list->string` get the corresponding string
-    (pure (syntax-box (list->string (syntax-box-datum id)) (syntax-box-srcloc id)))))
-
-(define expr/call/p
-  (do (keyword/p "apply")
-    (must/lexeme/p)
-    [e* <- (many/p expr/p #:sep must/lexeme/p)]
-    (pure (call (car e*) (cdr e*)))))
-(define unary/p
-  (do [expr <- (or/p (try/p expr/call/p)
-                     (try/p (syntax-box/p (or/p (string/p "true") (string/p "false"))))
-                     identifier/p
-                     (syntax-box/p integer/p))]
-    (lexeme/p)
-    (pure expr)))
-(define (binary/p high-level/p op-list)
-  (do [e <- high-level/p]
-    [es <- (many/p (do [op <- (or/p (one-of/p op-list) void/p)]
-                     (lexeme/p)
-                     [e <- high-level/p]
-                     (pure (list op e))))]
-    (pure (foldl
-           (λ (op+rhs lhs)
-             (match op+rhs
-               [(list op rhs)
-                (binary op lhs rhs)]))
-           e es))))
-(define (table/p base/p list-of-op-list)
-  (if (empty? list-of-op-list)
-      base/p
-      (table/p (binary/p base/p (car list-of-op-list)) (cdr list-of-op-list))))
-(define expr/p
-  (table/p unary/p
-           '((#\* #\/)
-             (#\+ #\-)
-             ("==" ))))
-
-(define binding/p
-  (do (keyword/p "let")
-    [name <- identifier/p]
-    (keyword/p "=")
-    [expr <- expr/p]
-    (pure (let name expr))))
-
-(parse-string binding/p "let a= apply foo 1")
+(parse #'(let a 1))
+(parse #'(let id (λ (x) x)))
+(parse #'(let a-id (id a)))
