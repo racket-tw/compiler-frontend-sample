ba759f4aac3ca798a5af6b4771fbb5cdf1e6beb8
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-12-26 23:09:14 +0800

    dep and build update

--- info.rkt
+++ info.rkt
@@ -1,9 +1,7 @@
 #lang info
 (define collection "compiler-frontend-sample")
 (define deps '("base"
-               "nanopass"
-               "megaparsack-lib"
-               "functional-lib"))
+               "nanopass"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/compiler-frontend-sample.scrbl" ())))
 (define pkg-desc "Description Here")
